ab0151d2048220a21d9256e693a32a0459d12e61
Aminul Ali <alomamin963@gmail.com>
2024-03-29 14:02:27 +0530

    Home page modified

--- app/page.tsx
+++ app/page.tsx
@@ -18,12 +18,16 @@ export default function Home() {
    }
   }
   return (
-   <>
-   <h1>I am Home</h1>
+   <div  className=" h-screen w-screen bg-teal-900">
+   <div className="flex flex-col justify-center items-center pt-[5rem]">
+   <h1 className="text-6xl text-white">I am Home</h1>
    <button className="bg-zinc-300 px-2 py-2 mx-4 my-4 rounded-md "
    onClick={logoutHandler} >Logout</button>
+   </div>
 
-   </>
+   </div>
+
+   
         
   )
 }
